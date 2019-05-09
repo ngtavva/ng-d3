ad1baabf39c8da0bb7c41ad32aca12e3e95cf670
chandra <chandra@tavva.in>
2019-05-09 12:54:24 +0530

    ngx-charts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,91 @@ export class AppComponent {
   lineColors = LineColors;
   nestedPieData = NestedPieData;
 
+
+  // data goes here
+public single = [
+  {
+    "name": "Germany",
+    "value": 8940000
+  },
+  {
+    "name": "USA",
+    "value": 5000000
+  },
+  {
+    "name": "France",
+    "value": 7200000
+  }
+];
+
+public multi = [
+  {
+    "name": "Germany",
+    "series": [
+      {
+        "name": "2010",
+        "value": 7300000
+      },
+      {
+        "name": "2011",
+        "value": 8940000
+      }
+    ]
+  },
+
+  {
+    "name": "USA",
+    "series": [
+      {
+        "name": "2010",
+        "value": 7870000
+      },
+      {
+        "name": "2011",
+        "value": 8270000
+      }
+    ]
+  },
+
+  {
+    "name": "France",
+    "series": [
+      {
+        "name": "2010",
+        "value": 5000002
+      },
+      {
+        "name": "2011",
+        "value": 5800000
+      }
+    ]
+  }
+];
+
+
+  view: any[] = [700, 400];
+
+  // options for the chart
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Number';
+  showYAxisLabel = true;
+  yAxisLabel = 'Value';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+
+  //pie
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+
 }
